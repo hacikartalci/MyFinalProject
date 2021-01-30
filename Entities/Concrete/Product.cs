999d37a2dfe2697cb75634b2d0ccb176ddfaf4fd
Haci Halil Kartalci <haci.kartalci@gmail.com>
2021-01-31 00:25:49 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //internal(default) sadece Entities erişebilir.
+    //public diğer katmanlarda erişebilir.
+    //short int'in bir küçüğüdür.
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
