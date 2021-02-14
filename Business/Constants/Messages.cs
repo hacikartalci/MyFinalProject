bcee14c86da4eccf0791f0d41be5200a3bd28fb5
Haci Halil Kartalci <haci.kartalci@gmail.com>
2021-02-14 22:52:09 +0300

    Yazılım Geliştirici Yetiştime Kampı 10. Gün C# Kurumsal Yazılım Mimarileri

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //static verirsen new'lemene ihtiyaç kalmaz.
+    //publicler pascal case yazılır. Baş harfler Büyük.
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        internal static string MaintenanceTime = "Sistem bakımdadır.";
+        internal static string ProductListed = "Ürünler listelendi.";
+    }
+}
